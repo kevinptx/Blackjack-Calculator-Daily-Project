c209260ea406fb5bd85dd39241722213fa41046d
kevinptx <kevin.peery@gmail.com>
2017-08-02 16:06:41 -0500

    completed daily blackjack project

--- scripts/main.js
+++ scripts/main.js
@@ -5,15 +5,28 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
 
 function handValue (hand) {
-
-
-  return;
+  let sum = 0
+  for (var i = 0; i < hand.length; i++) {
+  if ((hand[i] === "K") || (hand[i] === "Q") || (hand[i] === "J")) {
+    sum += 10
+  } else if (hand[i] === "A") {
+    sum += 1
+  } else {
+    sum += parseInt(hand[i])
+  }
+}
+for (var j = 0; j < hand.length; j++) {
+  if (hand[j]=== "A" && sum < 12)
+  sum += 10
+}
+console.log(sum);
+  return sum;
 }
 
 
